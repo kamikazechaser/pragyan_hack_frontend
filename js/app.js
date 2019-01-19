66883b8e3c70687b531f8b7a1ca3efeaed94ca7f
Mohammed Sohail <sohailsameja@gmail.com>
2019-01-19 21:46:01 +0530

    Angular Controller Structure

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+function supportsLocalStorage() {
+    return typeof (Storage) !== 'undefined';
+}
+
+if (!supportsLocalStorage()) {
+    alert("Upgrade your browser to use this service.")
+} else {
+    console.log("localStorage available!")
+}
+
+function pushToStore(key, value) {
+    return localStorage.setItem(key, value);
+}
+
+function getFromStore(key) {
+    return localStorage.getItem(key);
+}
+
+function clearStore() {
+    return localStorage.clear();
+}
 
 const theGuides = angular.module("theGuides", ["ngRoute"]);
 
@@ -27,10 +48,43 @@ theGuides.config(function ($routeProvider) {
         });
 });
 
+theGuides.controller("clientSignupController", function($scope, $http) {
+    $scope.subReg = function () {
+        // omegalul
+        const firstName = $scope.first_name;
+        const lastName = $scope.last_name;
+        const email = $scope.email;
+        const password = $scope.password;
+        const age = $scope.age;
+        const address = $scope.address;
+        const interests = $scope.interests;
+        const phone = $scope.phone;
+        const sex = $scope.sex;
+        $http.get(`http://127.0.0.1:3000/registerclient?email=${email}&name=${firstName} ${lastName}password=${password}&age=${age}&address=${address}&interests=${interests}&phone=${phone}&sex=${sex}`)
+            .then((ctx) => {
+                console.log("OK");
+                //const statusCode = ctx.data.status;
+              //  const message = ctx.data.message;
+
+                if (statusCode === 200) {
+                    //toastr.success(message);
+                    //$location.path("/login");
+                } else {
+                    //toastr.error(message);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                //let error = ctx.data;
+                //toastr.error(error);
+            });
+    }    
+});
+
 theGuides.controller("homeController", function ($scope, $http) {
     $scope.home = {};
 
-    $scope.searchLocation = function() {
+    $scope.searchLocation = function () {
         $http.get(`http://photon.komoot.de/api/?q=${$scope.home.autocomplete}`).then(response => {
             const responseArray = response.data.features;
             $scope.items = responseArray.splice(0, 5);
